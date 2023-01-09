1defc6707092d2f7d21ccb0878c4b1ed405fef7f
Starrick2001 <lebuidihoa257@gmail.com>
2023-01-10 03:45:47 +0700

    send req to BE

--- src/components/LoginGoogleButton/LoginGoogleButton.tsx
+++ src/components/LoginGoogleButton/LoginGoogleButton.tsx
@@ -1,11 +1,40 @@
-import { GoogleLogin } from "@react-oauth/google";
+import { CredentialResponse, GoogleLogin } from "@react-oauth/google";
+import axios from "axios";
 import jwtDecode from "jwt-decode";
+import { API_URL } from "../../utils/utils";
+
+type LoginSuccessRequest = {
+	email: string;
+	name: string;
+	given_name: string;
+	family_name: string;
+	picture: string;
+};
 
 export function LoginGoogleButton() {
+	const loginSuccessHandler = (credentialResponse: CredentialResponse) => {
+		const reqBody: LoginSuccessRequest = jwtDecode(
+			credentialResponse.credential as string
+		);
+		console.log(reqBody);
+
+		axios
+			.post(API_URL + "/auth", {
+				email: reqBody.email,
+				name: reqBody.name,
+				givenName: reqBody.given_name,
+				familyName: reqBody.family_name,
+				picture: reqBody.picture
+			})
+			.then((result) => {
+				console.log(result);
+			});
+	};
+
 	return (
 		<GoogleLogin
 			onSuccess={(credentialResponse) => {
-				console.log(jwtDecode(credentialResponse.credential as string));
+				loginSuccessHandler(credentialResponse);
 			}}
 			onError={() => {
 				console.log("Login Failed");
