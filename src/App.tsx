6b377233e03b4f20712545dba668a5346126dc0c
Starrick2001 <lebuidihoa257@gmail.com>
2023-01-09 20:27:28 +0700

    remove redundant file

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,5 @@
-import React, { useEffect } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import axios from "axios";
-import { API_URL } from "./utils/utils";
-
 function App() {
-	useEffect(() => {
-		axios.get(API_URL + "/auth").then((result) => {
-			console.log(result);
-		});
-	}, []);
-
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
-	);
+	return <></>;
 }
 
 export default App;
