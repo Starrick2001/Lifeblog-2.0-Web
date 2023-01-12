ae53198c072c95bff45edf744ba266aa065b7668
Starrick2001 <lebuidihoa257@gmail.com>
2023-01-13 01:28:06 +0700

    add profile page

--- src/routes/Profile/Profile.tsx
+++ src/routes/Profile/Profile.tsx
@@ -1,5 +1,53 @@
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/rootReducer";
+
 export function Profile() {
-  return (
-    <h4>Profile</h4>
-  )
+	const userData = useSelector((state: RootState) => state.userData.userData);
+
+	return (
+		<div className="container flex justify-center my-4 mx-auto">
+			<div className="bg-white shadow-lg rounded-2xl w-80 dark:bg-gray-800">
+				<img
+					alt="profile"
+					src="/landscape-cover.jpg"
+					className="w-full mb-4 rounded-t-lg h-auto"
+				/>
+				<div className="flex flex-col items-center justify-center p-4 -mt-16">
+					<button className="relative block">
+						<img
+							alt="profile"
+							src={userData?.picture}
+							className="mx-auto object-cover rounded-full h-16 w-16  border-2 border-white dark:border-gray-800"
+						/>
+					</button>
+					<p className="mt-2 text-xl font-medium text-gray-800 dark:text-white">
+						{userData?.name}
+					</p>
+					<p className="p-2 px-4 text-xs text-white bg-pink-500 rounded-full">
+						Professional
+					</p>
+					<div className="w-full p-2 mt-4 rounded-lg">
+						<div className="flex items-center justify-between text-sm text-gray-600 dark:text-gray-200">
+							<p className="flex flex-col">
+								Articles
+								<span className="font-bold text-black dark:text-white">34</span>
+							</p>
+							<p className="flex flex-col">
+								Friends
+								<span className="font-bold text-black dark:text-white">
+									455
+								</span>
+							</p>
+							<p className="flex flex-col">
+								Rating
+								<span className="font-bold text-black dark:text-white">
+									9.3
+								</span>
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
