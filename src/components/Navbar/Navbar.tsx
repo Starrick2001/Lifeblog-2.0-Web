99a8abd382c15aa939972cac53c2e5ad8da201bb
Starrick2001 <lebuidihoa257@gmail.com>
2023-01-12 23:48:40 +0700

    add dropdown, navigate to profile page

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,42 @@
+import { Menu, Transition } from "@headlessui/react";
+import { Fragment, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import { RootState } from "../../redux/rootReducer";
 import { setAuthToken } from "../../redux/slices/authTokenSlice";
+import { GetUserDataFromAccessToken } from "../../services/user.service";
+import { classNames } from "../../utils/utils";
 import { LoginGoogleButton } from "../LoginGoogleButton/LoginGoogleButton";
+
+interface UserData {
+	email: string;
+	name: string;
+	familyName: string;
+	givenName: string;
+	picture: string;
+}
 
 export function Navbar() {
 	const authToken = useSelector((state: RootState) => state.authToken);
 	const dispatch = useDispatch();
+	const [userData, setUserData] = useState<UserData>();
 
-	const logoutHandler = () => {
+	const signoutHandler = () => {
 		dispatch(setAuthToken(undefined));
 	};
+
+	useEffect(() => {
+		if (authToken.accessToken) {
+			const userData: any = GetUserDataFromAccessToken(authToken.accessToken);
+			setUserData({
+				email: userData.email,
+				familyName: userData.familyName, 
+				givenName: userData.givenName,
+				name: userData.name,
+				picture: userData.picture
+			});
+		} else setUserData(undefined);
+	}, [authToken.accessToken]);
 
 	return (
 		<header className="z-40 items-center w-full h-16 bg-white shadow-lg dark:bg-gray-700 rounded-2xl">
@@ -48,17 +75,69 @@ export function Navbar() {
 						</div>
 					</div>
 					<div className="relative flex items-center justify-end w-1/4 p-1 ml-5 mr-4 sm:mr-0 sm:right-auto">
-						{authToken.accessToken ? (
-							<button className="relative block" onClick={logoutHandler}>
-								<img
-									alt="profil"
-									src="/images/person/1.jpg"
-									className="mx-auto object-cover rounded-full h-10 w-10 "
-								/>
-							</button>
-						) : (
-							<LoginGoogleButton />
-						)}
+						<Menu as="div" className="relative inline-block text-left">
+							<div>
+								<Menu.Button>
+									{userData ? (
+										<button className="relative block">
+											<img
+												alt="profil"
+												src={userData.picture}
+												className="mx-auto object-cover rounded-full h-10 w-10 "
+											/>
+										</button>
+									) : (
+										<LoginGoogleButton />
+									)}
+								</Menu.Button>
+							</div>
+
+							<Transition
+								as={Fragment}
+								enter="transition ease-out duration-100"
+								enterFrom="transform opacity-0 scale-95"
+								enterTo="transform opacity-100 scale-100"
+								leave="transition ease-in duration-75"
+								leaveFrom="transform opacity-100 scale-100"
+								leaveTo="transform opacity-0 scale-95"
+							>
+								<Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+									<div className="py-1">
+										<Menu.Item>
+											{({ active }) => (
+												<Link
+												to='/profile'
+													className={classNames(
+														active
+															? "bg-gray-100 text-gray-900"
+															: "text-gray-700",
+														"block px-4 py-2 text-sm w-full text-left"
+													)}
+												>
+													Profile
+												</Link>
+											)}
+										</Menu.Item>
+										<Menu.Item>
+											{({ active }) => (
+												<button
+													onClick={signoutHandler}
+													type="submit"
+													className={classNames(
+														active
+															? "bg-gray-100 text-gray-900"
+															: "text-gray-700",
+														"block w-full px-4 py-2 text-left text-sm"
+													)}
+												>
+													Sign out
+												</button>
+											)}
+										</Menu.Item>
+									</div>
+								</Menu.Items>
+							</Transition>
+						</Menu>
 					</div>
 				</div>
 			</div>
